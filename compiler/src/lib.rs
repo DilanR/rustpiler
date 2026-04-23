90ebb56c2faf29dd8a547ca3bf131e1414882025
Dilan Redha <Dilanredha@gmail.com>
2026-04-23 17:04:46 +0200

    refactor: split into 2 workspaces

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -2,12 +2,10 @@
 pub mod common;
 pub mod error;
 pub mod intrinsics;
-pub mod test_util;
 
 // AST related
 pub mod ast;
 pub mod ast_traits;
-pub mod cli;
 pub mod parse;
 
 pub mod code_gen;
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use rnr::cli::Cli;
-
-fn main() {
-    let cli = Cli::parse();
-    if let Err(err) = cli.execute() {
-        eprintln!("{err}");
-        std::process::exit(1);
-    }
-}
